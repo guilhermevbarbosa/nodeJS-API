213c603eb25213ff06d040de04925ae053d8e5f8
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2021-03-26 22:04:47 -0300

    helmet

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,20 @@
 import express from "express";
 import cors from "cors";
 import "express-async-errors";
+import helmet from "helmet";
+
+require("dotenv/config");
 
 import "./database/connection";
 import routes from "./routes";
 import errorHandler from "./errors/errorHandler";
-require("dotenv/config");
 
 const app = express();
 
 app.use(cors());
 app.disable("x-powered-by");
+app.use(helmet());
+
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
